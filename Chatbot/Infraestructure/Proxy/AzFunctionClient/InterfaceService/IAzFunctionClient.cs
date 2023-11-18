6c9bd533b34f0c5486940c65fd507c011bb8bc63
Jeremias Fernandez <jfernandez@algeiba.com>
2023-11-18 13:46:35 -0300

    Initial project load

---
+++ Chatbot/Infraestructure/Proxy/AzFunctionClient/InterfaceService/IAzFunctionClient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzFunctionClient.InterfaceService
+{
+    public interface IAzFunctionClient
+    {
+        Task<string> SendMessageToLLM(string text);
+    }
+}
---
+++ Chatbot/Infraestructure/Proxy/AzFunctionClient/Service/AzFunctionClientService.cs
@@ -0,0 +1,26 @@
+﻿using AzFunctionClient.InterfaceService;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzFunctionClient.Service
+{
+    public class AzFunctionClientService : IAzFunctionClient, IDisposable
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ILogger<AzFunctionClientService> _logger;
+
+        public AzFunctionClientService(HttpClient httpClient, ILogger<AzFunctionClientService> logger) => (_httpClient, _logger) = (httpClient, logger);
+
+        public void Dispose() => _httpClient?.Dispose();
+
+        public async Task<string> SendMessageToLLM(string text)
+        {
+
+            return "";
+        }
+    }
+}
